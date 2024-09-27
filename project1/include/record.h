c1975d70707e6acb42e70c65b90405f98757f85a
Pu Fanyi <pufanyi@gmail.com>
2024-09-27 20:12:59 +0800

    record

--- project1/include/record.h
+++ project1/include/record.h
@@ -2,7 +2,21 @@
 #define RECORD_H
 
 #include "data_ptr.h"
+#include "schema.h"
 
-class Record : public DataPtr {};
+class Record : public DataPtr {
+ private:
+  std::shared_ptr<Schema> _schema;
+
+ public:
+  Record(BlockPtr &block_ptr, off_t offset, std::shared_ptr<Schema> schema)
+      : DataPtr(block_ptr, offset), _schema(schema) {}
+
+  Record(const Record &record) = default;
+
+  std::size_t size() override { return _schema->row_size(); }
+
+  friend std::ostream &operator<<(std::ostream &os, const Record &record);
+};
 
 #endif  // RECORD_H
---
+++ project1/src/record.cpp
@@ -0,0 +1,30 @@
+#include "record.h"
+
+#include <iomanip>
+
+constexpr std::size_t MIN_TABLE_WIDTH = 6;
+
+std::ostream &operator<<(std::ostream &os, const Record &record) {
+  std::size_t ptr = 0;
+  const Byte *bytes = record.getBytes();
+  std::vector<std::string> names;
+  std::vector<std::string> data;
+  std::vector<std::size_t> sizes;
+  for (const auto [name, field] : record._schema->dtypes()) {
+    names.push_back(name);
+    std::string value = field->bytesToString(bytes + ptr);
+    data.push_back(value);
+    sizes.push_back(
+        std::max(MIN_TABLE_WIDTH, std::max(name.size(), value.size())));
+    ptr += field->getSize();
+  }
+  os << "|";
+  for (decltype(names.size()) i = 0; i < names.size(); i++) {
+    os << " " << std::setw(sizes[i]) << names[i] << " |";
+  }
+  os << "\n|";
+  for (decltype(data.size()) i = 0; i < data.size(); i++) {
+    os << " " << std::setw(sizes[i]) << data[i] << " |";
+  }
+  return os;
+}
