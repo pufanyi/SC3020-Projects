339e7492200afa7d49c4a63f10b97b8562b1d126
kcz358 <92624596+kcz358@users.noreply.github.com>
2024-10-01 17:02:56 +0800

    Complete db manager test case

--- project1/test/test_manager.cpp
+++ project1/test/test_manager.cpp
@@ -8,4 +8,11 @@ TEST(ManagerTest, DBManagerTest) {
       "../assets/games.txt", "games",
       "DATE, VARCHAR(10), INT, FLOAT32, FLOAT32, FLOAT32, INT, INT, BOOLEAN",
       "\t");
+  EXPECT_EQ(records.size(), 26651);
+  EXPECT_EQ(records[0].toString(),
+            "22/12/2022 1610612740 126 0.484000 0.926000 0.382000 25 46 1 ");
+  EXPECT_EQ(records[1].toString(),
+            "22/12/2022 1610612762 120 0.488000 0.952000 0.457000 16 40 1 ");
+  EXPECT_EQ(records[2].toString(),
+            "21/12/2022 1610612739 114 0.482000 0.786000 0.313000 22 37 1 ");
 }
