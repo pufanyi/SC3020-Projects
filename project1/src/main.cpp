3d829c4b85b85cee818aa52c4a056d26f40e1382
kcz358 <92624596+kcz358@users.noreply.github.com>
2024-10-07 10:19:39 +0800

    Revise io printing messages

--- project1/src/main.cpp
+++ project1/src/main.cpp
@@ -14,13 +14,12 @@ int main() {
                    "\t");
   std::cout << db.get_records()[0] << std::endl;
   std::string index_name = "FG_PCT_home.idx";
-  std::cout << "IO Times Before Building Index: " << IO_TIMES << std::endl;
-  std::cout << "Load PTR Times Before Building Index: " << LOAD_PTR_TIMES
-            << std::endl;
+  size_t before;
+  size_t after;
+  before = IO_TIMES;
   auto b_tree = db.build_index(index_name, "FG_PCT_home", IndexType::FLOAT32);
-  std::cout << "IO Times After Building Index: " << IO_TIMES << std::endl;
-  std::cout << "Load PTR Times After Building Index: " << LOAD_PTR_TIMES
-            << std::endl;
+  after = IO_TIMES;
+  std::cout << "IO Times From Building Index: " << after - before << std::endl;
 
   std::shared_ptr<Index> lower_bound_index =
       std::make_shared<Float32Index>(0.5 - 1e-6);
@@ -28,20 +27,21 @@ int main() {
       std::make_shared<Float32Index>(0.8 + 1e-6);
 
   auto start = std::chrono::high_resolution_clock::now();
+  before = IO_TIMES;
   auto result = b_tree->range_query(lower_bound_index, upper_bound_index);
+  after = IO_TIMES;
   auto stop = std::chrono::high_resolution_clock::now();
   auto duration =
       std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
   auto micro_duration =
       std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 
-  std::cout << "IO Times After Range Query: " << IO_TIMES << std::endl;
-  std::cout << "Load PTR Times After Range Query: " << LOAD_PTR_TIMES
-            << std::endl;
+  size_t io_from_range_query = after - before;
   std::cout << "Result size: " << result.size() << std::endl;
 
   float sum = 0;
   float FG3_PCT_home_sum = 0;
+  before = IO_TIMES;
   for (auto record : result) {
     std::string value = record.toString();
     std::vector<std::string> splitted_values = split(value, ' ');
@@ -51,6 +51,12 @@ int main() {
     float FG3_PCT_home_value = std::stof(splitted_values[5]);
     FG3_PCT_home_sum += FG3_PCT_home_value;
   }
+  after = IO_TIMES;
+  size_t io_from_accessing_result = after - before;
+  std::cout << "IO TImes From Range Query: "
+            << io_from_range_query - io_from_accessing_result << std::endl;
+  std::cout << "IO Times From Accessing Result: " << io_from_accessing_result
+            << std::endl;
 
   std::cout << "Average FG_PCT_home: " << sum / result.size() << std::endl;
   std::cout << "Average FG3_PCT_home: " << FG3_PCT_home_sum / result.size()
