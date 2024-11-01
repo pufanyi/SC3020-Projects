1e5998e4b981060d838f8330ef4a30f616e5facc
kcz358 <92624596+kcz358@users.noreply.github.com>
2024-11-01 21:22:32 +0800

    Mount gradio to server

--- project2/src/sc3020/interface.py
+++ project2/src/sc3020/interface.py
@@ -1,5 +1,6 @@
 import os
 
+import gradio as gr
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -12,10 +13,21 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 
-
+CUSTOM_PATH = "/gradio"
 templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
 async def main(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+with gr.Blocks() as demo:
+    gr.Markdown("## Demo")
+    with gr.Row(equal_height=True) as row:
+        text_input = gr.Textbox(
+            lines=5, label="Input Text", placeholder="Enter some text here"
+        )
+        button = gr.Button("Submit", variant="primary")
+
+app = gr.mount_gradio_app(app, demo, path=CUSTOM_PATH)
