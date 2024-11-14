218379371bf7082cc99d9a78123fcc94c95abd5e
kcz358 <92624596+kcz358@users.noreply.github.com>
2024-11-14 21:44:58 +0800

    Reformat Download data

--- project2/src/utils/data_downloader.py
+++ project2/src/utils/data_downloader.py
@@ -1,7 +1,13 @@
-from tqdm import tqdm
+import os
 from pathlib import Path
-from datasets import load_dataset, get_dataset_config_names, Dataset
-from typing import Dict, List
+from typing import Dict, List, Union
+
+from datasets import Dataset, get_dataset_config_names, load_dataset
+from tqdm import tqdm
+
+# Get the full path of current file
+PATH_FILE = os.path.dirname(os.path.abspath(__file__))
+path = Path(PATH_FILE)
 
 
 class TPCHDataset(object):
@@ -11,9 +17,14 @@ class TPCHDataset(object):
         for subset in tqdm(self.subsets, desc="Loading subsets"):
             self.data[subset] = load_dataset(hf_path, subset, split="train")
 
-    def save(self, path: str | Path = Path(__file__).parents[1] / "assets" / "data"):
-        path.mkdir(parents=True, exist_ok=True)
+    def save(self, path: Union[str, Path]):
         if isinstance(path, str):
             path = Path(path)
+        path.mkdir(parents=True, exist_ok=True)
         for subset in tqdm(self.subsets, desc="Saving subsets"):
             self.data[subset].to_csv(path / f"{subset}.csv", index=False)
+
+
+if __name__ == "__main__":
+    tpch = TPCHDataset()
+    tpch.save(path.parent / "sc3020" / "assets" / "data")
