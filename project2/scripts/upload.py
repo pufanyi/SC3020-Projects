cdf5c95287980394fbee9612007e231959c96a14
Pu Fanyi <pufanyi@gmail.com>
2024-11-15 10:48:34 +0800

    我靠 github 爆密码了

--- project2/scripts/upload.py
+++ project2/scripts/upload.py
@@ -3,13 +3,24 @@ from pathlib import Path
 
 import pandas as pd
 from datasets import Dataset
+from huggingface_hub import HfApi
 
 if __name__ == "__main__":
     folder = Path(__file__).parents[1] / "assets" / "data"
-    with open("headers.json") as f:
+    with open(Path(__file__).parent / "headers.json", "r") as f:
         headers = json.load(f)
+
+    api = HfApi()
+
     for file in folder.glob("*.tbl"):
         header = headers[file.stem]
         data = pd.read_table(file, sep="|", names=header)
         hf_data = Dataset.from_pandas(data, preserve_index=False)
         hf_data.push_to_hub(f"pufanyi/TPC-H", file.stem)
+
+        api.upload_file(
+            path_or_fileobj=file,
+            path_in_repo=f"raw_data/{file.name}",
+            repo_id="pufanyi/TPC-H",
+            repo_type="dataset",
+        )
